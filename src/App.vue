a9389426a2cee09e097138a4c5963284ba3cdb40
Razz21 <razzrazz@wp.pl>
2020-09-26 11:46:22 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <control v-model="val"></control>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      val: 0
+    };
   }
-}
+};
 </script>
 
 <style lang="scss">
+* {
+  box-sizing: border-box;
+  position: relative;
+}
+body {
+  -webkit-transition: 0s;
+  transition: 0s;
+  background-color: #181b1c;
+  font-family: monospace;
+  color: #e4e8ea;
+  font-size: 16px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -1,10 +1,31 @@
-import Vue from 'vue'
-import App from './App.vue'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import store from "./store";
 
-Vue.config.productionTip = false
+/*
+===========================
+>>> Globally register all components from 'src/components/global' directory
+===========================
+*/
+const files = require.context("@/components/global", false, /\.vue$/i);
+
+files.keys().map(key => {
+  Vue.component(
+    files(key).default.name ??
+      key
+        .split("/")
+        .pop()
+        .split(".")[0],
+    files(key).default
+  );
+});
+/*
+===========================
+*/
+
+Vue.config.productionTip = false;
 
 new Vue({
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
