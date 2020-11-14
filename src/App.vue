bb68ff4152cc62f9edf93639d6ad90d70639b359
Razz21 <razzrazz@wp.pl>
2020-11-14 11:58:37 +0100

    fix component render

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
 <template>
   <div id="app">
-    <div style="z-index:3; position:absolute;top:0;left:0">
-      <button @click="dumpPreset">Dump Preset</button>
-      <button @click="panic">Panic</button>
-      <button @click="toggleAnalyser">wave</button>
-    </div>
     <div class="flex m-auto" style="z-index:5">
-      <Synthesizer ref="synth" v-show="loaded" />
+      <Synthesizer />
     </div>
   </div>
 </template>
@@ -15,31 +10,7 @@
 import Synthesizer from "@/components/Synthesizer";
 export default {
   name: "App",
-  components: { Synthesizer },
-  data() {
-    return {
-      loaded: false
-    };
-  },
-  methods: {
-    dumpPreset() {
-      this.$refs.synth.dumpPreset();
-    },
-    async panic() {
-      this.$refs.synth.panic();
-    },
-    toggleAnalyser() {
-      this.$refs.synth.toggleAnalyser();
-    }
-  },
-  mounted() {
-    window.addEventListener("load", () => {
-      this.$nextTick(() => {
-        this.loaded = true;
-      });
-      console.log("app loaded");
-    });
-  }
+  components: { Synthesizer }
 };
 </script>
 
